147383498c19d91295a26b7ee57cb707a7f07fef
hamzaalalach <hamzaalalach@gmail.com>
2016-11-27 10:57:32 +0000

    Add 2 players logic

--- scripts/base.js
+++ scripts/base.js
@@ -17,26 +17,71 @@ pixels = document.getElementById(id).querySelectorAll('.pixel');
 }
 function o(id) {
 pixels = document.getElementById(id).querySelectorAll('.pixel');
-	pixels[2].style.backgroundColor = '#FFF';
-	pixels[6].style.backgroundColor = '#FFF';
-	pixels[10].style.backgroundColor = '#FFF';
-	pixels[16].style.backgroundColor = '#FFF';
-	pixels[22].style.backgroundColor = '#FFF';
-	pixels[18].style.backgroundColor = '#FFF';
-	pixels[14].style.backgroundColor = '#FFF';
-	pixels[8].style.backgroundColor = '#FFF';
+	pixels[2].style.backgroundColor = '#000';
+	pixels[6].style.backgroundColor = '#000';
+	pixels[10].style.backgroundColor = '#000';
+	pixels[16].style.backgroundColor = '#000';
+	pixels[22].style.backgroundColor = '#000';
+	pixels[18].style.backgroundColor = '#000';
+	pixels[14].style.backgroundColor = '#000';
+	pixels[8].style.backgroundColor = '#000';
 }
+
+/* Init main logic */
 var divs = document.querySelectorAll('.block'),
 	divsL = divs.length,
-	c = 0;
+	c = 0,
+	process = {b1: '', b2: '', b3: '', b4: '', b5: '', b6: '', b7: '', b8: '', b9: ''};
 for (var i = 0; i < divsL; i++) {
 	divs[i].addEventListener('click', function(e) {
-		if (c == 0) {
-			o(this.id);
-			c = 1;
-		} else {
-			x(this.id);
-			c = 0;
+		if(process['b' + this.id] === '') {
+			if (c == 0) {
+				o(this.id);
+				c = 1;
+				var t = 'b' + this.id;
+				process[t] = 'o';
+				check();
+			} else {
+				x(this.id);
+				c = 0;
+				var t = 'b' + this.id;
+				process[t] = 'x';
+				check();
+			}
 		}
 	}, false);
 }
+function check() {
+	if (process.b1 === process.b2 && process.b2 === process.b3 && process.b1 != '') {
+		alert(process.b1 + ' wins');
+	} else if (process.b4 === process.b5 && process.b5 === process.b6 && process.b4 != '') {
+		alert(process.b4 + ' wins')
+	} else if (process.b7 === process.b8 && process.b8 === process.b9 && process.b7 != '') {
+		alert(process.b7 + ' wins')
+	} else if (process.b1 === process.b4 && process.b4 === process.b7 && process.b1 != '') {
+		alert(process.b1 + ' wins')
+	} else if (process.b2 === process.b5 && process.b5 === process.b8 && process.b2 != '') {
+		alert(process.b2 + ' wins')
+	} else if (process.b3 === process.b6 && process.b6 === process.b9 && process.b3 != '') {
+		alert(process.b3 + ' wins')
+	} else if (process.b1 === process.b5 && process.b5 === process.b9 && process.b1 != '') {
+		alert(process.b1 + ' wins')
+	} else if (process.b3 === process.b5 && process.b5 === process.b7 && process.b3 != '') {
+		alert(process.b3 + ' wins')
+	} else if (process.b1 != '' && process.b2 != '' && process.b3 != '' && process.b4 != '' && process.b5 != '' && process.b6 != '' && process.b7 != '' && process.b8 != '' && process.b9 != '') {
+		alert('Draw!');
+	}
+}
+
+/* Init tools */
+var newGame = document.getElementById('newGame');
+newGame.addEventListener('click', function() {
+	for (var i in process) {
+		process[i] = '';
+	}
+	var pixels = document.querySelectorAll('.pixel'),
+		pixelsL = pixels.length;
+	for (var i = 0; i < pixelsL; i++) {
+		pixels[i].style.backgroundColor = 'transparent';
+	}
+}, false);
